d6d56c6b74ce92b025a44fac270b1ef223c88db7
Peter Covington <petercov@outlook.com>
2022-09-12 19:21:50 -0400

    Misc Additions

--- sp/src/game/shared/weapon_parse.cpp
+++ sp/src/game/shared/weapon_parse.cpp
@@ -34,7 +34,8 @@ const char *pWeaponSoundCategories[ NUM_SHOOT_SOUND_TYPES ] =
 	"special2",
 	"special3",
 	"taunt",
-	"deploy"
+	"deploy",
+	"pump_npc",
 };
 #else
 extern const char *pWeaponSoundCategories[ NUM_SHOOT_SOUND_TYPES ];
@@ -70,7 +71,7 @@ itemFlags_t g_ItemFlags[8] =
 	{ "ITEM_FLAG_NOITEMPICKUP",		ITEM_FLAG_NOITEMPICKUP }
 };
 #else
-extern itemFlags_t g_ItemFlags[7];
+extern itemFlags_t g_ItemFlags[8];
 #endif
 
 
