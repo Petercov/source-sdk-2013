42034aeaaa7c4176cd95ebc2825e571c7edc47c6
Peter Covington <petercov@outlook.com>
2023-02-21 18:34:35 -0500

    Fix SiN hologram actor

--- sp/src/game/server/sin/npc_generic_actor.cpp
+++ sp/src/game/server/sin/npc_generic_actor.cpp
@@ -293,18 +293,87 @@ public:
 	int		GetSoundInterests(void);
 	bool	UseSemaphore(void);
 
-	virtual void StartTouch(CBaseEntity* pOther);
-	virtual void EndTouch(CBaseEntity* pOther);
-
-private:
-	int		m_nTouchCount;
+	void	SetInterference(bool bEnable);
 };
 
 BEGIN_DATADESC(CNPC_SinHologram)
-DEFINE_FIELD(m_nTouchCount, FIELD_INTEGER),
 END_DATADESC();
 
 LINK_ENTITY_TO_CLASS(npc_hologram, CNPC_SinHologram);
+
+class CHologramTrigger : public CBaseEntity
+{
+public:
+	DECLARE_CLASS(CHologramTrigger, CBaseEntity);
+	DECLARE_DATADESC();
+
+	// 
+	// Creates a trigger with the specified bounds
+
+	static CHologramTrigger* Create(const Vector& vecOrigin, const Vector& vecMins, const Vector& vecMaxs, CNPC_SinHologram* pOwner)
+	{
+		CHologramTrigger* pTrigger = (CHologramTrigger*)CreateEntityByName("trigger_sin_hologram");
+		if (pTrigger == NULL)
+			return NULL;
+
+		UTIL_SetOrigin(pTrigger, vecOrigin);
+		UTIL_SetSize(pTrigger, vecMins, vecMaxs);
+		pTrigger->SetOwnerEntity(pOwner);
+		pTrigger->SetParent(pOwner);
+
+		pTrigger->Spawn();
+
+		return pTrigger;
+	}
+
+	//
+	// Setup the entity
+
+	void Spawn(void)
+	{
+		BaseClass::Spawn();
+
+		SetSolid(SOLID_BBOX);
+		SetSolidFlags(FSOLID_TRIGGER | FSOLID_NOT_SOLID);
+	}
+
+	void Activate()
+	{
+		BaseClass::Activate();
+		SetSolidFlags(FSOLID_TRIGGER | FSOLID_NOT_SOLID); // Fixes up old savegames
+	}
+
+	virtual void StartTouch(CBaseEntity* pOther) {
+		if (CanTouch(pOther))
+		{
+			if (m_nTouchCount == 0)
+				GetHologram()->SetInterference(true);
+
+			m_nTouchCount++;
+		}
+	}
+	virtual void EndTouch(CBaseEntity* pOther) {
+		if (CanTouch(pOther))
+		{
+			m_nTouchCount--;
+
+			if (m_nTouchCount == 0)
+				GetHologram()->SetInterference(false);
+		}
+	}
+
+private:
+	inline CNPC_SinHologram* GetHologram() { return static_cast<CNPC_SinHologram*> (GetOwnerEntity()); }
+	bool CanTouch(CBaseEntity* pOther);
+
+	int		m_nTouchCount;
+};
+
+BEGIN_DATADESC(CHologramTrigger)
+DEFINE_FIELD(m_nTouchCount, FIELD_INTEGER),
+END_DATADESC();
+
+LINK_ENTITY_TO_CLASS(trigger_sin_hologram, CHologramTrigger);
 
 //-----------------------------------------------------------------------------
 // Classify - indicates this NPC's place in the 
@@ -337,7 +406,7 @@ void CNPC_SinHologram::Spawn()
 	SetHullSizeNormal();
 
 	SetSolid(SOLID_BBOX);
-	AddSolidFlags(FSOLID_NOT_STANDABLE|FSOLID_NOT_SOLID|FSOLID_TRIGGER);
+	AddSolidFlags(FSOLID_NOT_STANDABLE|FSOLID_NOT_SOLID);
 	SetMoveType(MOVETYPE_STEP);
 	SetBloodColor(DONT_BLEED);
 	m_iHealth = 8;
@@ -352,6 +421,8 @@ void CNPC_SinHologram::Spawn()
 	NPCInit();
 
 	m_takedamage = DAMAGE_NO;
+
+	CHologramTrigger::Create(GetAbsOrigin(), Vector(-15, -15, 0), Vector(15, 15, 72), this);
 }
 
 //-----------------------------------------------------------------------------
@@ -372,26 +443,34 @@ bool CNPC_SinHologram::UseSemaphore(void)
 	return BaseClass::UseSemaphore();
 }
 
-void CNPC_SinHologram::StartTouch(CBaseEntity* pOther)
+bool CHologramTrigger::CanTouch(CBaseEntity* pOther)
 {
-	if (m_nTouchCount == 0)
+	if (!pOther || pOther == GetOwnerEntity() || pOther->GetMoveType() == MOVETYPE_NONE || !pOther->IsSolid())
+		return false;
+
+	if (pOther->GetFlags() & (FL_NPC | FL_CLIENT))
+		return true;
+
+	if (pOther->GetMoveType() == MOVETYPE_VPHYSICS)
+		return true;
+
+	return false;
+}
+
+void CNPC_SinHologram::SetInterference(bool bEnable)
+{
+	if (bEnable)
 	{
 		m_nRenderFX = kRenderFxHologram;
 		m_nRenderMode = kRenderTransTexture;
 		SetRenderColorA(200);
+		m_nSkin = 1;
 	}
-
-	m_nTouchCount++;
-}
-
-void CNPC_SinHologram::EndTouch(CBaseEntity* pOther)
-{
-	m_nTouchCount--;
-
-	if (m_nTouchCount == 0)
+	else
 	{
 		m_nRenderFX = kRenderFxNone;
 		m_nRenderMode = kRenderNormal;
 		SetRenderColorA(255);
+		m_nSkin = 0;
 	}
 }
